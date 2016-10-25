1157346095faadcaa4b38d5fd38b4a9d60b00cbd
Ostap <fantom2012@ukr.net>
2016-10-25 17:34:25 +0300

    slide to

--- agritask/js/init.js
+++ agritask/js/init.js
@@ -11,24 +11,31 @@ $(document).ready(function(){
     ;
   });
 
-$('.owl-carousel').owlCarousel({
+  $('.owl-carousel').owlCarousel({
     loop:true,
     margin:0,
     nav:true,
     dots: true,
     responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
+      0:{
+        items:1
+      },
+      600:{
+        items:1
+      },
+      1000:{
+        items:1
+      }
     }
-})
-
+  })
+  $('.see-below').click(function() {
+    $(this).removeAttr("href");
+    $("html, body").animate({ scrollTop: $(".field-data-icons").offset().top}, 1000);
+  });
+  $('.see-product').click(function() {
+    $(this).removeAttr("href");
+    $("html, body").animate({ scrollTop: $(".agritask-for-wrap").offset().top}, 1000);
+  });
 
 });
 
